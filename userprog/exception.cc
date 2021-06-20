0cd44e04fd1be30673078d295245fe0dca38704b
cheng128 <chuchuuu128@gmail.com>
2021-06-20 11:27:11 +0800

    test

--- userprog/exception.cc
+++ userprog/exception.cc
@@ -56,95 +56,95 @@ ExceptionHandler(ExceptionType which)
 	int	val, status;
 
     switch (which) {
-	case SyscallException:
-	{
-	    switch(type) {
-		case SC_Halt:
-		    DEBUG(dbgAddr, "Shutdown, initiated by user program.\n");
-   		    kernel->interrupt->Halt();
-		    break;
-		case SC_PrintInt:
-			val=kernel->machine->ReadRegister(4);
-			cout << "Print integer:" <<val << endl;
-			return;
-/*		case SC_Exec:
-			DEBUG(dbgAddr, "Exec\n");
-			val = kernel->machine->ReadRegister(4);
-			kernel->StringCopy(tmpStr, retVal, 1024);
-			cout << "Exec: " << val << endl;
-			val = kernel->Exec(val);
-			kernel->machine->WriteRegister(2, val);
-			return;
-*/		case SC_Exit:
-			DEBUG(dbgAddr, "Program exit\n");
-			val=kernel->machine->ReadRegister(4);
-			cout << "return value:" << val << endl;
-			kernel->currentThread->Finish();
-			break;
-
-		case SC_Msg:
+		case SyscallException:
 		{
-			//DEBUG(dbgSys, "Message received.\n");
-			val = kernel->machine->ReadRegister(4);
+			switch(type) {
+			case SC_Halt:
+				DEBUG(dbgAddr, "Shutdown, initiated by user program.\n");
+				kernel->interrupt->Halt();
+				break;
+			case SC_PrintInt:
+				val=kernel->machine->ReadRegister(4);
+				cout << "Print integer:" <<val << endl;
+				return;
+	/*		case SC_Exec:
+				DEBUG(dbgAddr, "Exec\n");
+				val = kernel->machine->ReadRegister(4);
+				kernel->StringCopy(tmpStr, retVal, 1024);
+				cout << "Exec: " << val << endl;
+				val = kernel->Exec(val);
+				kernel->machine->WriteRegister(2, val);
+				return;
+	*/		case SC_Exit:
+				DEBUG(dbgAddr, "Program exit\n");
+				val=kernel->machine->ReadRegister(4);
+				cout << "return value:" << val << endl;
+				kernel->currentThread->Finish();
+				break;
+
+			case SC_Msg:
 			{
-				char *msg = &(kernel->machine->mainMemory[val]);
-				cout << msg << endl;
+				//DEBUG(dbgSys, "Message received.\n");
+				val = kernel->machine->ReadRegister(4);
+				{
+					char *msg = &(kernel->machine->mainMemory[val]);
+					cout << msg << endl;
+				}
+				kernel->interrupt->Halt();
+				ASSERTNOTREACHED();
+				break;
 			}
-			kernel->interrupt->Halt();
-			ASSERTNOTREACHED();
+
+			case SC_Create:
+				val = kernel->machine->ReadRegister(4);
+				{
+					char *filename = &(kernel->machine->mainMemory[val]);
+					status = kernel->fileSystem->Create(filename);	
+					kernel->machine->WriteRegister(2, (int)status);
+				}
+				return;
+				ASSERTNOTREACHED();
+				break;
+			default:
+				cerr << "Unexpected system call " << type << "\n";
+				break;
+			}
+			return;
+			break;
+		}
+		case PageFaultException:
+		{
+			unsigned int vpn = kernel->machine->ReadRegister(BadVAddrReg)/PageSize;
+			cout << "VirtualAdd: " << vpn << endl;
+			cout << "currentThread->name: " << kernel->currentThread->getName() << endl;
+			cout << "currentThread->space: " << kernel->currentThread->space << endl;
+			kernel->currentThread->space->pageFault(vpn);
+			cout << "return Exception" << endl;
+
+			return;
+		}
+		
+		case AddressErrorException:
+		{
+			cout << "Address Error Exception" << endl;	
 			break;
 		}
 
-		case SC_Create:
-			val = kernel->machine->ReadRegister(4);
-			{
-				char *filename = &(kernel->machine->mainMemory[val]);
-				status = kernel->fileSystem->Create(filename);	
-				kernel->machine->WriteRegister(2, (int)status);
-			}
-			return;
-			ASSERTNOTREACHED();
+		case ReadOnlyException:
+		{
+			cout << "Read Only Exception" << endl;
 			break;
+		}
+		case BusErrorException:
+		{
+			cout << "BusErrorException" << endl;
+			break;
+		}
+			//HW3>
+
 		default:
-		    cerr << "Unexpected system call " << type << "\n";
- 		    break;
-	    }
-	    return;
-		break;
+			cerr << "Unexpected user mode exception" << which << "\n";
+			break;
 	}
-	case PageFaultException:
-	{
-		unsigned int vpn = kernel->machine->ReadRegister(BadVAddrReg)/PageSize;
-		cout << "VirtualAdd: " << vpn << endl;
-		cout << "currentThread->name: " << kernel->currentThread->getName() << endl;
-		cout << "currentThread->space: " << kernel->currentThread->space << endl;
-		kernel->currentThread->space->pageFault(vpn);
-		cout << "return Exception" << endl;
-
-		break;
-	}
-	
-	case AddressErrorException:
-	{
-		cout << "Address Error Exception" << endl;	
-		break;
-	}
-
-	case ReadOnlyException:
-	{
-		cout << "Read Only Exception" << endl;
-		break;
-	}
-	case BusErrorException:
-	{
-		cout << "BusErrorException" << endl;
-		break;
-	}
-		//HW3>
-
-	default:
-	    cerr << "Unexpected user mode exception" << which << "\n";
-	    break;
-    }
     ASSERTNOTREACHED();
 }
