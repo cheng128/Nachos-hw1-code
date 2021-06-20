8d42efd0b23e88b05946ef89b340387c1985c9af
cheng128 <chuchuuu128@gmail.com>
2021-06-20 14:01:04 +0800

    test

--- userprog/addrspace.cc
+++ userprog/addrspace.cc
@@ -300,16 +300,18 @@ int AddrSpace::AllocPage(AddrSpace* space, int vpn)
 int AddrSpace::FindFreePage()
 {
     cout << "in FindFreePage function" << endl;
-
-    for(unsigned int i; i<NumPhysPages; i++)
+    for(unsigned int i=0; i<NumPhysPages; i++)
     {
         if(kernel->machine->PhyPageStatus[i]==FALSE)
         {
-            cout << "PAGE NOT USED" << endl;
-             kernel->machine->PhyPageStatus[i] = TRUE;
+            kernel->machine->PhyPageStatus[i] = TRUE;
             return i;
+            break;
         }
-
+        else
+        {
+            cout << "No Free Page" << endl;
+        }
     }
     return -1;
 }
