c1cb07f295d8e0ccc9bc759819a9a8aa357b21fe
cheng128 <chuchuuu128@gmail.com>
2021-06-20 13:26:45 +0800

    test

--- test/shell.c
+++ test/shell.c
@@ -3,7 +3,7 @@
 int
 main()
 {
-	/*
+	
     SpaceId newProc;
     OpenFileId input = ConsoleInput;
     OpenFileId output = ConsoleOutput;
@@ -31,6 +31,6 @@ main()
 		newProc = Exec(buffer);
 		Join(newProc);
 	}
-    }*/
+    }
 }
 
