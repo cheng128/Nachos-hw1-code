9c0620ce5e7e194e4746aab788f31d2cba50da25
cheng128 <chuchuuu128@gmail.com>
2021-06-20 14:53:45 +0800

    test

--- userprog/exception.cc
+++ userprog/exception.cc
@@ -48,99 +48,73 @@
 //	are in machine.h.
 //----------------------------------------------------------------------
 
-
 void
 ExceptionHandler(ExceptionType which)
 {
 	int	type = kernel->machine->ReadRegister(2);
 	int	val, status;
-	cout << "type: " << type << endl;
+
     switch (which) {
-		case SyscallException:
+	case SyscallException:
+	    switch(type) {
+		case SC_Halt:
+		    DEBUG(dbgAddr, "Shutdown, initiated by user program.\n");
+   		    kernel->interrupt->Halt();
+		    break;
+		case SC_PrintInt:
+			val=kernel->machine->ReadRegister(4);
+			cout << "Print integer:" <<val << endl;
+			return;
+/*		case SC_Exec:
+			DEBUG(dbgAddr, "Exec\n");
+			val = kernel->machine->ReadRegister(4);
+			kernel->StringCopy(tmpStr, retVal, 1024);
+			cout << "Exec: " << val << endl;
+			val = kernel->Exec(val);
+			kernel->machine->WriteRegister(2, val);
+			return;
+*/		case SC_Exit:
+			DEBUG(dbgAddr, "Program exit\n");
+			val=kernel->machine->ReadRegister(4);
+			cout << "return value:" << val << endl;
+			kernel->currentThread->Finish();
+			break;
+
+		case SC_Msg:
 		{
-			cout << "in SyscallException" << endl;
-			cout << "in SyscallException type: " << type << endl;
-			switch(type) {
-				cout << "in switch" << endl;
-				case SC_Halt:
-					DEBUG(dbgAddr, "Shutdown, initiated by user program.\n");
-					kernel->interrupt->Halt();
-					break;
-				case SC_PrintInt:
-					cout << "in SC_PrintInt" << endl;
-					val=kernel->machine->ReadRegister(4);
-					cout << "Print integer:" <<val << endl;
-					return;
-		/*		case SC_Exec:
-					DEBUG(dbgAddr, "Exec\n");
-					val = kernel->machine->ReadRegister(4);
-					kernel->StringCopy(tmpStr, retVal, 1024);
-					cout << "Exec: " << val << endl;
-					val = kernel->Exec(val);
-					kernel->machine->WriteRegister(2, val);
-					return;
-		*/		case SC_Exit:
-					DEBUG(dbgAddr, "Program exit\n");
-					val=kernel->machine->ReadRegister(4);
-					cout << "return value:" << val << endl;
-					kernel->currentThread->Finish();
-					break;
-
-				case SC_Msg:
-				{
-					//DEBUG(dbgSys, "Message received.\n");
-					val = kernel->machine->ReadRegister(4);
-					{
-						char *msg = &(kernel->machine->mainMemory[val]);
-						cout << msg << endl;
-					}
-					kernel->interrupt->Halt();
-					ASSERTNOTREACHED();
-					break;
-				}
-
-				case SC_Create:
-					val = kernel->machine->ReadRegister(4);
-					{
-						char *filename = &(kernel->machine->mainMemory[val]);
-						status = kernel->fileSystem->Create(filename);	
-						kernel->machine->WriteRegister(2, (int)status);
-					}
-					return;
-					ASSERTNOTREACHED();
-					break;
-				default:
-					cerr << "Unexpected system call " << type << "\n";
-					break;
+			//DEBUG(dbgSys, "Message received.\n");
+			val = kernel->machine->ReadRegister(4);
+			{
+				char *msg = &(kernel->machine->mainMemory[val]);
+				cout << msg << endl;
 			}
+			kernel->interrupt->Halt();
+			ASSERTNOTREACHED();
 			break;
 		}
-		case PageFaultException:
-		{
-		}
-		break;
-		case AddressErrorException:
-		{
-			cout << "Address Error Exception" << endl;	
-			
-		}
-		break;	
-		case ReadOnlyException:
-		{
-			cout << "Read Only Exception" << endl;
-		}
-		break;
-		case BusErrorException:
-		{
-			cout << "BusErrorException" << endl;
-		}
-		break;
-			//HW3>
 
-		default:
-			cerr << "Unexpected user mode exception" << which << "\n";
+		case SC_Create:
+			val = kernel->machine->ReadRegister(4);
+			{
+				char *filename = &(kernel->machine->mainMemory[val]);
+				status = kernel->fileSystem->Create(filename);	
+				kernel->machine->WriteRegister(2, (int)status);
+			}
+			return;
+			ASSERTNOTREACHED();
 			break;
-	}
-	break;
+		//<TODO
+		
+		//TODO>
+		
+		default:
+		    cerr << "Unexpected system call " << type << "\n";
+ 		    break;
+	    }
+	    break;
+	default:
+	    cerr << "Unexpected user mode exception" << which << "\n";
+	    break;
+    }
     ASSERTNOTREACHED();
 }
