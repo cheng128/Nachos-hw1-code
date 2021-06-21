7d5d5360217c294b3a42459241164e8391c3ba94
cheng128 <chuchuuu128@gmail.com>
2021-06-21 10:04:02 +0800

    test

--- userprog/addrspace.cc
+++ userprog/addrspace.cc
@@ -362,6 +362,7 @@ int AddrSpace::SwapOut(int vpn)
     OpenFile *vm = kernel->fileSystem->Open("./test/vm");
     // if (vm)
     //     cout << "Open vm succeed" << endl;
+    cout << "swap out phy page: " << pageTable[vpn].physicalPage<< endl;
     // cout << "swap out phy address: " << pageTable[vpn].physicalPage * PageSize << endl;
     // cout << "swap out valid: " << pageTable[vpn].valid << endl;
     // cout << "pageTable[vpn].virtualPage * PageSize: " << pageTable[vpn].virtualPage * PageSize << endl;
