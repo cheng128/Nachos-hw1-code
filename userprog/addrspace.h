cbdd9728019d2309038a9f56cb75ab88c0bcfbcb
cheng128 <chuchuuu128@gmail.com>
2021-06-22 15:43:04 +0800

    test

--- userprog/addrspace.h
+++ userprog/addrspace.h
@@ -17,7 +17,7 @@
 #include "filesys.h"
 #include <string.h>
 
-#define UserStackSize		8192 	// increase this as necessary!
+#define UserStackSize		5168 	// increase this as necessary!
 
 class AddrSpace {
   public:
